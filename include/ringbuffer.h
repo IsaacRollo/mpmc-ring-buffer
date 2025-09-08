86b038cf8884b01dda6d3a4c9117a585755a4830
IsaacRollo <IsaacRollo@gmail.com>
2025-09-08 12:33:45 -0500

    read done

--- include/ringbuffer.h
+++ include/ringbuffer.h
@@ -30,7 +30,7 @@ RingBuffer(size_t size) :
 void write(const void* data, size_t length){
     
     auto size = static_cast<std::ptrdiff_t>(length);
-    char* preWriteBuffer;//gfh
+    char* preWriteBuffer;
 
     while(true){
         preWriteBuffer = _writeBuffer.load(std::memory_order_acquire);
@@ -48,9 +48,58 @@ void write(const void* data, size_t length){
     }
 
     if(preWriteBuffer + length < _endRing)
-        std::memcpy(preWriteBuffer, data, length);
+        memcpy(preWriteBuffer, data, length);
+    else{
+        size_t stubLength = _endRing - preWriteBuffer;
+        memcpy(preWriteBuffer, data, stubLength);
+        memcpy(_startRing, data + stubLength, length - stubLength);
+    }
 
+    while(preWriteBuffer != _writePointer.load());
 
+    auto postWriteBuffer = preWriteBuffer + size;
+    postWriteBuffer = postWriteBuffer < _endRing ? postWriteBuffer : postWriteBuffer - (_endRing - _startRing);
+
+    _writePointer.store(postWriteBuffer);
+    _usedSpace.fetch_add(length, std::memory_order_relaxed);
+
+}
+
+void read(void* data, std::size_t length){
+
+    auto size = static_cast<std::ptrdiff_t>(length);
+    char* preReadBuffer;
+
+    while(true){
+        preReadBuffer = _readBuffer.load(std::memory_order_consume);
+        while(_usedSpace.load(std::memory_order_consume) < size);
+
+        auto postReadBuffer = preReadBuffer + size;
+        postReadBuffer = postReadBuffer < _endRing ? postReadBuffer : postReadBuffer - (_endRing - _startRing);
+        _usedSpace.fetch_sub(size);
+
+        if(_readPointer.compare_exchange_strong(preReadBuffer, postReadBuffer))
+            break;
+        
+        _usedSpace.fetch_add(size, std::memory_order_relaxed);
+    }
+    
+    if(preReadBuffer + length < _endRing){
+        memcpy(data, preReadBuffer, length);
+    }
+    else{
+        auto stubLength = _endRing - preReadBuffer;
+        memcpy(data, preReadBuffer, stubLength);
+        memcpy(data + stubLength, _startRing, length - stubLength);
+    }
+
+    auto postReadBuffer = preReadBuffer + size;
+    postReadBuffer = postReadBuffer < _endRing ? postReadBuffer : postReadBuffer - (_endRing - _startRing);
+
+    while(_readPointer.load() != preReadBuffer);
+
+    _readPointer.store(postReadBuffer);
+    _freeSpace.fetch_add(length, std::memory_order_relaxed);
 
 }
 
